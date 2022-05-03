817f4c90a10d82c771f5600aa0dc9aedc4fba9d3
Markus Koechl <markus.koechl@autodesk.com>
2022-05-03 22:36:47 +0200

    Add project files.

---
+++ VDS-Sample-Utilities-2023/VdsSampleAdminForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace VdsSampleUtilities
+{
+    partial class VdsSampleAdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // VdsSampleAdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 356);
+            this.IconOptions.Image = global::VdsSampleUtilities.Properties.Resources.Open_Settings_16_Light;
+            this.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.Name = "VdsSampleAdminForm";
+            this.Text = "VDS-Sample-Administration";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
